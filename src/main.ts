73a5fd5a99c436586634380c752166ec27044b23
Simon Kelly <simonkellly@gmail.com>
2022-11-03 12:21:49 +0000

    Implement the website

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,164 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { Competition } from "@wca/helpers";
+import "./index.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const compIdElem = document.querySelector<HTMLInputElement>("#comp-id")!;
+const passwordsElem = document.querySelector<HTMLInputElement>("#passwords")!;
+const sortButtonElem = document.querySelector<HTMLButtonElement>("#sort")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface Dictionary<T> {
+  [Key: string]: T;
+}
+
+const fullNames: Dictionary<string> = {
+  "2x2x2": "222",
+  "3x3x3": "333",
+  "4x4x4": "444",
+  "5x5x5": "555",
+  "6x6x6": "666",
+  "7x7x7": "777",
+  "3x3x3 Blindfolded": "333bf",
+  "3x3x3 Fewest Moves": "333fm",
+  "3x3x3 One-Handed": "333oh",
+  Clock: "clock",
+  Megaminx: "minx",
+  Pyraminx: "pyram",
+  Skewb: "skewb",
+  "Square-1": "sq1",
+  "4x4x4 Blindfolded": "444bf",
+  "5x5x5 Blindfolded": "555bf",
+  "3x3x3 Multiple Blindfolded": "333mbf",
+};
+
+const eventNames: Dictionary<string> = {
+  "222": "2x2",
+  "333": "3x3",
+  "444": "4x4",
+  "555": "5x5",
+  "666": "6x6",
+  "777": "7x7",
+  "333bf": "3BLD",
+  "333fm": "FMC",
+  "333oh": "OH",
+  clock: "Clock",
+  minx: "Mega",
+  pyram: "Pyra",
+  skewb: "Skewb",
+  sq1: "SQ-1",
+  "444bf": "4BLD",
+  "555bf": "5BLD",
+  "333mbf": "MBLD",
+};
+
+interface Event {
+  event: string;
+  roundId: number;
+}
+
+function parseEventId(id: string): Event | null {
+  if (!id) return null;
+  if (id.length < 3) return null;
+  const [event, round] = id.split("-");
+
+  if (!Object.keys(eventNames).includes(event)) return null;
+  const roundId = parseInt(round.substring(1)) ?? null;
+  if (roundId == null || isNaN(roundId)) return null;
+
+  return { event, roundId };
+}
+
+interface Password {
+  password: string;
+  event: string;
+  round: number;
+}
+
+function parsePassword(password: string): Password | undefined {
+  const eventEnd = password.indexOf(" Round");
+  if (eventEnd === -1) return undefined;
+
+  const event = password.substring(0, eventEnd);
+  const round = password.substring(eventEnd + 7, eventEnd + 8);
+  const eventId = fullNames[event];
+  const shortEventName = eventNames[eventId];
+  const roundId = parseInt(round);
+
+  if (!eventId || !shortEventName || !roundId) return undefined;
+
+  const passwordString =
+    shortEventName +
+    " R" +
+    password
+      .substring(eventEnd + 7)
+      .replace(" Scramble Set ", " ")
+      .replace(" Attempt ", "-A");
+
+  return {
+    password: passwordString,
+    event: eventId,
+    round: roundId,
+  };
+}
+
+async function getWCIF(competitionId: string): Promise<Competition | undefined> {
+  const apiUrl = `https://worldcubeassociation.org/api/v0/competitions/${competitionId}/wcif/public`;
+  const response = await fetch(apiUrl);
+  
+  if (!response.ok) {
+    return undefined;
+  }
+
+  const wcif = await response.json();
+  return wcif as Competition;
+}
+
+async function sortPasswords() {
+  const competitionId = compIdElem.value.replace(/\s/g, "");
+  const wcif = await getWCIF(competitionId);
+
+  if (!wcif) {
+    alert("Error fetching competition details");
+    return;
+  }
+
+  const passwords = passwordsElem.value;
+
+  const activities = wcif.schedule.venues
+    ?.flatMap((venue) => venue.rooms)
+    .flatMap((room) => room.activities);
+
+  const sorted = activities!
+    .sort((a, b) => Date.parse(a.startTime) - Date.parse(b.startTime))
+    .map((activity) => {
+      return parseEventId(activity.activityCode);
+    })
+    .filter((event) => event) as Event[];
+
+  const parsedPasswords = passwords
+    .split("\n")
+    .filter((p) => p)
+    .map((password) => {
+      return parsePassword(password);
+    })
+    .filter((password): password is Password => !!password)
+
+  const sortedPasswords = parsedPasswords
+    .sort((a: Password, b: Password) => {
+      const firstIdx = sorted.findIndex(
+        (event) => event!.event === a.event && event.roundId === a.round
+      );
+      const secondIdx = sorted.findIndex(
+        (event) => event!.event === b.event && event.roundId === b.round
+      );
+
+      return firstIdx - secondIdx;
+    })
+    .map((password) => password.password);
+
+  passwordsElem.value = sortedPasswords.join("\r\n");
+}
+
+sortButtonElem.onclick = async () => {
+  sortButtonElem.classList.add("loading")
+  await sortPasswords();
+  sortButtonElem.classList.remove("loading")
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["emerald"],
+  },
+}
