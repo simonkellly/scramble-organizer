6fadeb0882a7aebd922d760fd1725f0b015039c8
Simon Kelly <simonkellly@gmail.com>
2023-07-13 19:00:14 +0100

    Add additional message about new file drop feature

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,6 +5,9 @@ export default function Title() {
       <p className="text-2xl font-semibold">
         A simple tool to sort WCA scramble passwords.
       </p>
+      <p className="text-l font-semibold">
+        Try dropping your passwords file onto the page!
+      </p>
     </div>
   );
 }
